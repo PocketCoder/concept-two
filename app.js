4e6f5c0aa5ee4ceff38e9c117c06eb22cae1b645
PocketCoder <jake.williams77@gmail.com>
2020-05-27 17:42:32 +0100

    removed compression

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 var path = require('path');
 const bodyParser = require("body-parser");
 const nodemailer = require('nodemailer');
-const compression = require('compression');
 
 app.use(express.static('public'));
 
@@ -20,8 +19,6 @@ app.listen(process.env.PORT || 8000, () => {
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-app.use(compression());
 
 app.set("view engine", "ejs");
 
