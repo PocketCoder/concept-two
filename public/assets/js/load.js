2ad0bb108897e8ce7d16f4e007f01d5a9c507cda
PocketCoder <jake.williams77@gmail.com>
2020-05-28 09:40:04 +0100

    added load.js

---
+++ public/assets/js/load.js
@@ -0,0 +1,15 @@
+var rellax = new Rellax('.rellax');
+var rellaxC = new Rellax('.rellax-c', {
+    center: true
+});
+
+//TODO: https://medium.com/@aswin_s/fullscreen-video-backgrounds-e8376ef93c72 -- read through and complete before deployment
+const v = document.getElementById('video-bg');
+const hv = document.querySelector('.work--video-bg');
+hv.preload = "auto";
+
+v.addEventListener('canplaythrough', () => {
+    setTimeout(() => {
+        v.play();
+    });
+});
