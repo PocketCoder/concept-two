41a872b4f4a2915c22cb04f86502882b9cd2abb2
PocketCoder <jake.williams77@gmail.com>
2020-06-24 18:13:06 +0100

    testing exposed creds

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.zoho.eu',
     port: 465,
     auth: {
-        user: process.env.ZOHO_USER,
-        pass: 'XfU1ZCJ052mX' || process.env.ZOHO_PASS
+        user: 'jake@jpwfilm.co.uk', // || process.env.ZOHO_USER,
+        pass: 'XfU1ZCJ052mX' // || process.env.ZOHO_PASS
     }
 });
 
